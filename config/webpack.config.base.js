ce50b4218f63e9d5d6d04c210c989a16c9f87c65
Niklas Gruhn <niklas.gruhn@gmail.com>
2019-02-20 22:46:10 +0100

    build: remove ESLint in favor of Prettier

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -1,46 +1,42 @@
-var webpack = require('webpack')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-var outputFile = 'vue-qrcode-reader'
-var globalName = 'VueQrcodeReader'
+var outputFile = "vue-qrcode-reader";
+var globalName = "VueQrcodeReader";
 
-var config = require('../package.json')
+var config = require("../package.json");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   module: {
     rules: [
       {
-        enforce: 'pre',
-        test: /\.(js|vue)$/,
-        loader: 'eslint-loader',
-        exclude: /node_modules/,
-      },
-      {
         test: /.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
+        use: "babel-loader",
+        exclude: /node_modules/
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
+        loader: "vue-loader",
         options: {
           loaders: {
-            css: ExtractTextPlugin.extract({ use: {
-              loader: 'css-loader',
-              options: { minimize: true },
-            }}),
-            sass: ExtractTextPlugin.extract('css-loader!sass-loader'),
-            scss: ExtractTextPlugin.extract('css-loader!sass-loader'),
-          },
-        },
-      },
-    ],
+            css: ExtractTextPlugin.extract({
+              use: {
+                loader: "css-loader",
+                options: { minimize: true }
+              }
+            }),
+            sass: ExtractTextPlugin.extract("css-loader!sass-loader"),
+            scss: ExtractTextPlugin.extract("css-loader!sass-loader")
+          }
+        }
+      }
+    ]
   },
   plugins: [
     new webpack.DefinePlugin({
-      'VERSION': JSON.stringify(config.version),
+      VERSION: JSON.stringify(config.version)
     }),
-    new ExtractTextPlugin(outputFile + '.css'),
-  ],
-}
+    new ExtractTextPlugin(outputFile + ".css")
+  ]
+};
